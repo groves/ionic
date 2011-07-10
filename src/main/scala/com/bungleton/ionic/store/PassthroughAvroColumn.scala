481460dc2af4f3cba80341236ab325e6c7de7657
Charlie Groves <charlie.groves@gmail.com>
2011-07-10 15:19:18 -0700

    Handle all primitives

--- src/main/scala/com/bungleton/ionic/store/PassthroughAvroColumn.scala
+++ src/main/scala/com/bungleton/ionic/store/PassthroughAvroColumn.scala
@@ -1,17 +1,38 @@
 package com.bungleton.ionic.store
 
+import java.nio.ByteBuffer
+import org.apache.avro.util.Utf8
 import org.apache.avro.io.EncoderFactory
 import com.threerings.fisy.Directory
 import org.apache.avro.io.Decoder
 import org.apache.avro.Schema
 
+import org.apache.avro.Schema.Type._
+
+object PassthroughAvroColumn {
+  val passthroughTypes = Set(NULL, BOOLEAN, INT, LONG, FLOAT, DOUBLE, BYTES, STRING)
+}
 class PassthroughAvroColumn (decoder :Decoder, dest :Directory, field :Schema.Field)
     extends Column {
+  private var utf8Buf = new Utf8
+  private var byteBuf :ByteBuffer = null
   private val out = dest.open(field.name).write()
   private val encoder = EncoderFactory.get().directBinaryEncoder(out, null)
   private val writer = field.schema.getType match {
-    case Schema.Type.BOOLEAN => () => encoder.writeBoolean(decoder.readBoolean())
-    case Schema.Type.INT => () => encoder.writeInt(decoder.readInt())
+    case NULL => () => ()
+    case BOOLEAN => () => encoder.writeBoolean(decoder.readBoolean())
+    case INT => () => encoder.writeInt(decoder.readInt())
+    case LONG => () => encoder.writeLong(decoder.readLong())
+    case FLOAT => () => encoder.writeFloat(decoder.readFloat())
+    case DOUBLE => () => encoder.writeDouble(decoder.readDouble())
+    case STRING => () => {
+      utf8Buf = decoder.readString(utf8Buf)
+      encoder.writeString(utf8Buf)
+    }
+    case BYTES => () => {
+      byteBuf = decoder.readBytes(byteBuf)
+      encoder.writeBytes(byteBuf)
+    }
     case x => throw new IllegalArgumentException("Unknown schema type: " + x)
   }
   def write() { writer() }
--- src/test/scala/com/bungleton/ionic/store/WriteSimpleColumns.scala
+++ src/test/scala/com/bungleton/ionic/store/WriteSimpleColumns.scala
@@ -41,13 +41,13 @@ class WriteSimpleColumns {
     }}
   }
 
-  @Test def writeBooleans () = {
+  @Test def writeBooleans () {
     check[Boolean](List(("bool1", BOOLEAN, List(true)), ("bool2", BOOLEAN, List(false))),
       (enc, value) => enc.writeBoolean(value),
       (dec, values) => values.foreach(x => assertEquals(dec.readBoolean(), x)))
   }
 
-  @Test def writeInts () = {
+  @Test def writeInts () {
     check[Int](List(("int1", INT, List(0, 0, 0)),
       ("int2", INT, List(1, 2, 3)),
       ("int3", INT, List(101010, 10101010, 1010101010))),
@@ -55,7 +55,7 @@ class WriteSimpleColumns {
       (dec, values) => values.foreach(x => assertEquals(dec.readInt(), x)))
   }
 
-  @Test def writeSortedLong () = {
+  @Test def writeSortedLong () {
     check[Long](List(("timestamp", LONG,
       List(1000, 1000, 1000, 1000, 1001, 1001, 1001, 1002, 2002, 2002))),
       (enc, value) => enc.writeLong(value),
@@ -69,5 +69,47 @@ class WriteSimpleColumns {
         assertEquals(dec.readLong(), 1000)
         assertEquals(dec.readLong(), 2)
        })
+  }
+
+  @Test def writeString () {
+    check[String](List(("str", STRING, List("one", "two", "three"))),
+      (enc, value) => enc.writeString(value),
+      (dec, values) => values.foreach(x => assertEquals(dec.readString(null).toString, x)))
+  }
+
+  @Test def writeDouble () {
+    check[Double](List(("double", DOUBLE, List(0.1, 1289213.3281 -213232123.1))),
+      (enc, value) => enc.writeDouble(value),
+      (dec, values) => values.foreach(x => assertEquals(dec.readDouble(), x)))
+  }
+
+  @Test def writeLong () {
+    check[Long](List(("long", LONG, List(1234213123, 0, -848342834813232123l))),
+      (enc, value) => enc.writeLong(value),
+      (dec, values) => values.foreach(x => assertEquals(dec.readLong(), x)))
+  }
+
+  @Test def writeFloat () {
+    check[Float](List(("float", FLOAT, List(0.1f, 1289213.3281f, -2132.1f))),
+      (enc, value) => enc.writeFloat(value),
+      (dec, values) => values.foreach(x => assertEquals(dec.readFloat(), x)))
+  }
+
+  @Test def writeNull () {
+    check[Any](List(("null", NULL, List(()))),
+      (_, _) => (),
+      (_, _) => ())
+  }
+
+  @Test def writeBytes () {
+    check[Array[Byte]](List(("bytes", BYTES,
+      List(Array[Byte](1, 2, 3), Array[Byte](), Array[Byte](23, 43, 12)))),
+      (enc, value) => enc.writeBytes(value),
+      (dec, values) => values.foreach(x => {
+          val read = dec.readBytes(null)
+          assertEquals(read.limit, x.length)
+          (0 until read.limit).foreach(i => assertEquals(read.get(), x(i)))
+        }))
+
   }
 }
