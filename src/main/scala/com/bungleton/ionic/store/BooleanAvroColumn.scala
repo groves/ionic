60b26ecf158efd15c9c6d9fffc5b66ab919f5038
Charlie Groves <charlie.groves@gmail.com>
2011-07-09 21:30:14 -0700

    Cleanup

--- src/main/scala/com/bungleton/ionic/store/BooleanAvroColumn.scala
+++ src/main/scala/com/bungleton/ionic/store/BooleanAvroColumn.scala
@@ -1,11 +1,9 @@
 package com.bungleton.ionic.store
 
 import com.threerings.fisy.Directory
-import org.apache.avro.Schema
 import org.apache.avro.io.Decoder
-import com.threerings.fisy.Record
 
-class BooleanAvroColumn (val decoder :Decoder, val dest :Directory, val field :String) {
+class BooleanAvroColumn (decoder :Decoder, dest :Directory, field :String) {
   private val out = dest.open(field).write()
   def write() {
     out.write(if (decoder.readBoolean()) 1 else 0)
