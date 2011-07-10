d48cd35c52820c0ddd6b060365c11147e891b69c
Charlie Groves <charlie.groves@gmail.com>
2011-07-10 12:59:48 -0700

    Unify the column writing and checking

--- src/main/scala/com/bungleton/ionic/store/Writer.scala
+++ src/main/scala/com/bungleton/ionic/store/Writer.scala
@@ -8,7 +8,7 @@ import scala.collection.JavaConversions._
 
 class Writer (schema :Schema, decoder :Decoder, dest :Directory) {
   private val writers = schema.getFields.map(f =>
-      if (f.schema.getType == Schema.Type.FIXED && f.schema.getName() == "timestamp") {
+      if (f.schema.getType == Schema.Type.LONG && f.name == "timestamp") {
         new SortedLongColumn(decoder, dest, f)
       } else {
         new PassthroughAvroColumn(decoder, dest, f)
--- src/test/scala/com/bungleton/ionic/store/WriteSimpleColumns.scala
+++ src/test/scala/com/bungleton/ionic/store/WriteSimpleColumns.scala
@@ -15,19 +15,9 @@ import org.apache.avro.Schema
 import org.testng.annotations.Test
 
 import scala.collection.JavaConversions._
+import org.apache.avro.Schema.Type._
 
 class WriteSimpleColumns {
-  def encodeThenWrite (rec :Schema, records :Int, enc :(Encoder => Unit)) :Directory= {
-    val baos = new ByteArrayOutputStream
-    enc(EncoderFactory.get().directBinaryEncoder(baos, null))
-    val decoder = DecoderFactory.get().binaryDecoder(baos.toByteArray(), null)
-    val root = Paths.makeMemoryFs()
-    val writer = new Writer(rec, decoder, root)
-    0 until records foreach (_ => writer.write)
-    writer.close()
-    root
-  }
-
   def createRecord (name :String, fields :List[Schema.Field]) = {
     val record = Schema.createRecord(name, "", "", false)
     record.setFields(fields)
@@ -38,56 +28,55 @@ class WriteSimpleColumns {
     new Schema.Field(name, Schema.create(stype), "", null)
   }
 
-  @Test def writeBooleans () = {
-    val fields = List("bool1", "bool2")
-    val record = createRecord("TestBooleans", fields.map(createField(_, Schema.Type.BOOLEAN)))
+  def check[T] (defs :List[Tuple3[String, Schema.Type, List[T]]], enc :((Encoder, T) => Unit),
+    dec :((Decoder, List[T]) => Unit)) {
 
-    val root = encodeThenWrite(record, 1, enc => {
-      enc.writeBoolean(true);
-      enc.writeBoolean(false)
-    })
+    val baos = new ByteArrayOutputStream
+    val encoder = EncoderFactory.get().directBinaryEncoder(baos, null)
+    0 until defs(0)._3.length foreach(x => defs.foreach(f => enc(encoder, f._3(x))))
 
-    fields.zip(List(1, 0)).foreach { case (name, value) => {
+    val decoder = DecoderFactory.get().binaryDecoder(baos.toByteArray(), null)
+    val root = Paths.makeMemoryFs()
+    val rec = createRecord("testrec", defs.map(f => createField(f._1, f._2)))
+    val writer = new Writer(rec, decoder, root)
+    0 until defs(0)._3.length foreach (_ => writer.write)
+    writer.close()
+
+    defs.foreach { case (name, _, values) => {
       val in = root.open(name).read()
-      assertEquals(in.read(), value, name + " wrote the wrong value")
+      val decoder = DecoderFactory.get().binaryDecoder(in, null)
+      dec(decoder, values)
       assertEquals(in.read(), -1)
     }}
+  }
+
+  @Test def writeBooleans () = {
+    check[Boolean](List(("bool1", BOOLEAN, List(true)), ("bool2", BOOLEAN, List(false))),
+      (enc, value) => enc.writeBoolean(value),
+      (dec, values) => values.foreach(x => assertEquals(dec.readBoolean(), x)))
   }
 
   @Test def writeInts () = {
-    val fields = List("int1", "int2", "int3")
-    val record = createRecord("TestInts", fields.map(createField(_, Schema.Type.INT)))
-
-    val colValues = List(List(0, 0, 0), List(1, 2, 3), List(101010, 10101010, 1010101010))
-    val root = encodeThenWrite(record, 3, enc => {
-      0 until 3 foreach(x => colValues.foreach(vals => enc.writeInt(vals(x))))
-    })
-    fields.zip(colValues).foreach { case (name, values) => {
-      val in = root.open(name).read()
-      val dec = DecoderFactory.get().binaryDecoder(in, null)
-      values.foreach(v => assertEquals(dec.readInt(), v, name + " wrote the wrong value"))
-      assertEquals(in.read(), -1)
-    }}
+    check[Int](List(("int1", INT, List(0, 0, 0)),
+      ("int2", INT, List(1, 2, 3)),
+      ("int3", INT, List(101010, 10101010, 1010101010))),
+      (enc, value) => enc.writeInt(value),
+      (dec, values) => values.foreach(x => assertEquals(dec.readInt(), x)))
   }
 
   @Test def writeSortedLong () = {
-    val record = createRecord("TestTimestamp", List(createField("timestamp", Schema.Type.LONG)))
-
-    val colValues = List(1000, 1000, 1000, 1000, 1001, 1001, 1001, 1002, 2002, 2002)
-    val root = encodeThenWrite(record, colValues.length, enc => {
-      colValues.foreach(v=>enc.writeLong(v))
-    })
-    val in = new CountingInputStream(root.open("timestamp").read())
-    val dec = DecoderFactory.get().binaryDecoder(in, null)
-    assertEquals(dec.readLong(), 1000)
-    assertEquals(dec.readLong(), 4)
-    assertEquals(dec.readLong(), 1)
-    assertEquals(dec.readLong(), 3)
-    assertEquals(dec.readLong(), 1)
-    assertEquals(dec.readLong(), 1)
-    assertEquals(dec.readLong(), 1000)
-    assertEquals(dec.readLong(), 2)
-    assertEquals(in.read(), -1)
-    assertEquals(in.getCount, 10)
+    check[Long](List(("timestamp", LONG,
+      List(1000, 1000, 1000, 1000, 1001, 1001, 1001, 1002, 2002, 2002))),
+      (enc, value) => enc.writeLong(value),
+      (dec, values) => {
+        assertEquals(dec.readLong(), 1000)
+        assertEquals(dec.readLong(), 4)
+        assertEquals(dec.readLong(), 1)
+        assertEquals(dec.readLong(), 3)
+        assertEquals(dec.readLong(), 1)
+        assertEquals(dec.readLong(), 1)
+        assertEquals(dec.readLong(), 1000)
+        assertEquals(dec.readLong(), 2)
+       })
   }
 }
