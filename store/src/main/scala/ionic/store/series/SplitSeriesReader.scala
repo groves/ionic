6576d6569f19a2264e1e7b4e5dfad3e361593460
Charlie Groves <charlie.groves@gmail.com>
2011-12-31 11:14:35 -0800

    Comment what's happening in read

--- store/src/main/scala/ionic/store/series/SplitSeriesReader.scala
+++ store/src/main/scala/ionic/store/series/SplitSeriesReader.scala
@@ -25,9 +25,11 @@ class SplitSeriesReader(source: Directory, where: Where = Where())
   extends Iterator[GenericRecord] {
   private val schema = SeriesReader.readSchema(source)
   private val meta = SeriesReader.readMeta(source)
+  // Create a list of readers for each field with the conditions in the where for that field
   private val readers = schema.getFields.map(f => {
     val clausesForField = where.clauses.filter(_.f == f.name)
     if (f.schema.getType == LONG) {
+      // TODO - freak out if there are non-LongConds
       val conds = clausesForField.collect({ case l: LongCond => l })
       if (f.name == "timestamp") {
         new SortedLongColumnReader(source, f, meta.entries, conds)
@@ -66,23 +68,29 @@ class SplitSeriesReader(source: Directory, where: Where = Where())
 
   def read(old: GenericRecord = null): GenericRecord = {
     val record = if (old != null) { old } else { new GenericData.Record(schema) }
+    // Start with all the readers at the current position
     var positions = scala.collection.mutable.IndexedSeq.fill(readers.size)(-1L)
-    var initialPosition = 0L
-    var readPosition = 0L
+    var doPosition = 0L // The position at the start of the do loop
+    var readPosition = 0L // Records read in this call to read
     do {
-      initialPosition = readPosition
-      (0 until readers.size).takeWhile(_ => initialPosition == readPosition).
+      // Go through each of the readers not at readPosition and read until a matching value for
+      // all fields is found.
+      doPosition = readPosition
+      (0 until readers.size).takeWhile(_ => doPosition == readPosition).
         filter(positions(_) != readPosition).foreach({ idx: Int =>
+          // Skip up from the reader's position to readPosition and read the next match into record
           readers(idx).read(record, readPosition - positions(idx) - 1) match {
-            case None => throw NoneFound
+            case None => throw NoneFound // Off the end with no match
             case Some(read) => {
+              // Move up for the number of records read to the match. If we skip, bump
+              // readPosition to get the other readers to catch up.
               if (read > 1) readPosition += read - 1
               positions(idx) = readPosition
             }
           }
         })
-    } while (initialPosition != readPosition)
-    record
+    } while (doPosition != readPosition)
+    record // Made it all the way through for all the readers, record is filled with the values
   }
   def close() { readers.foreach(_.close()) }
 }
